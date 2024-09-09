ab8d6ada4a80c40da8d4240975dfe02f01219d75
Aiden Dong <dwx_job@163.com>
2024-09-10 00:32:37 +0800

    1

--- app/constant.ts
+++ app/constant.ts
@@ -281,12 +281,27 @@ const openaiModels = [
 const googleModels = [
   "gemini-1.5-flash-latest",
   "gemini-1.5-flash-001",
+  "gemini-1.5-flash-001-tuning",
   "gemini-1.5-flash",
+  "gemini-1.5-flash-exp-0827",
+  "gemini-1.5-flash-8b-exp-0827",
   "gemini-1.5-pro-latest",
+  "gemini-1.5-pro-001",
+  "gemini-1.5-pro",
   "gemini-1.5-pro-exp-0801",
+  "gemini-1.5-pro-exp-0827",
   "gemini-1.0-pro-latest",
-  "gemini-1.0-pro-001",
   "gemini-1.0-pro",
+  "gemini-pro",
+  "gemini-1.0-pro-001",
+  "gemini-1.0-pro-vision-latest",
+  "gemini-pro-vision",
+  "text-bison-001",
+  "text-embedding-004",
+  "chat-bison-001",
+  "embedding-001",
+  "embedding-gecko-001",
+  "aqa",
 ];
 
 const anthropicModels = [
