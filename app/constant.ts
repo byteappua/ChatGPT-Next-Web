cd272f4cba97f5a20172535b8ebdf0006042bae0
Aiden Dong <dwx_job@163.com>
2024-09-23 23:37:10 +0800

    1

--- app/constant.ts
+++ app/constant.ts
@@ -257,10 +257,10 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   "gemini-pro-vision": "2023-12",
 };
 
-export const DEFAULT_TTS_ENGINE = "OpenAI-TTS";
+export const DEFAULT_TTS_ENGINE = "Edge-TTS";
 export const DEFAULT_TTS_ENGINES = ["OpenAI-TTS", "Edge-TTS"];
 export const DEFAULT_TTS_MODEL = "tts-1";
-export const DEFAULT_TTS_VOICE = "alloy";
+export const DEFAULT_TTS_VOICE = "echo";
 export const DEFAULT_TTS_MODELS = ["tts-1", "tts-1-hd"];
 export const DEFAULT_TTS_VOICES = [
   "alloy",
